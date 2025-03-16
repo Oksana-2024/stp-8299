06326e7b2c845574e12be3f4639166c9dddb3720
Oksana <oksanavasilevna77@gmail.com>
2025-03-16 16:19:54 +0200

    add section faq, library accordion, localStorage-api.js

---
+++ src/js/faq-accordion.js
@@ -0,0 +1,10 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.js-accordion', {
+  elementClass: 'js-accordion-item',
+  triggerClass: 'js-accordion-trigger',
+  panelClass: 'js-accordion-body',
+  activeClass: 'active',
+  openOnInit: [],
+});
