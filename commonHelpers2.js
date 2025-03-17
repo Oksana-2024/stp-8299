90bf31e70d4520891f09c788eab77325b4be0863
Oksana-2024 <oksanavasilevna77@gmail.com>
2025-03-17 11:12:26 +0000

    Deploying to gh-pages from @ Oksana-2024/stp-8299@a3dae8278ec150c694e4a664d8696ba4dd47c71c 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/scroll-9b53d0c2.js";import{A as o}from"./assets/vendor-44e78a51.js";new o(".js-accordion",{elementClass:"js-accordion-item",triggerClass:"js-accordion-trigger",panelClass:"js-accordion-body",activeClass:"active",openOnInit:[]});
+import"./assets/scroll-fa180900.js";import{A as o}from"./assets/vendor-44e78a51.js";new o(".js-accordion",{elementClass:"js-accordion-item",triggerClass:"js-accordion-trigger",panelClass:"js-accordion-body",activeClass:"active",openOnInit:[]});
 //# sourceMappingURL=commonHelpers2.js.map
