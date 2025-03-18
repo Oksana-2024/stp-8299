4bd6dc17fa168cbc19f3fad85617fb3db4d8f647
Oksana <oksanavasilevna77@gmail.com>
2025-03-18 10:51:09 +0200

    feat: gallery section

---
+++ src/js/gallery.js
@@ -0,0 +1,35 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+
+
+let swiper;
+function initSwiper() {
+  if (window.innerWidth < 1200) {
+    if (!swiper) {
+      swiper = new Swiper('.js-gallery-swiper', {
+        direction: 'horizontal',
+        centerInsufficientSlides: true,
+        spaceBetween: 24,
+        autoHeight: true,
+        slidesPerView: 1,
+        pagination: {
+          // Пагінація (точки)
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        modules: [Pagination],
+        /*  breakpointsBase: 'container', */
+        breakpoints: {
+          1200: {
+            enabled: false, // Вимкнення слайдера при 1200px і більше
+          },
+        },
+      });
+    }
+  }
+}
+
+window.addEventListener('resize', initSwiper);
+initSwiper();
