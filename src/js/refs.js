502d3ebfa06ce7cce59accecf1f912e6f7409759
Oksana <oksanavasilevna77@gmail.com>
2025-03-19 11:00:31 +0200

    add images for mobile menu, fix styles gallery section, cookies page

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,7 @@
 export const refs = {
-  scrollBtn: document.querySelector(".js-scroll-btn"),
-}
+  scrollBtn: document.querySelector('.js-scroll-btn'),
+  menuBtn: document.querySelector('.js-menu-open'),
+  mobileMenu: document.querySelector('.js-mob-menu'),
+  closeBtn: document.querySelector('.js-mob-menu-close'),
+  menuLinks: document.querySelectorAll('.js-nav-link'),
+};
